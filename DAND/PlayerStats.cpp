fb456a3db687f024a30c407062ed5f56555927d5
michael <Michael_Ralston@yahoo.com>
2015-09-23 12:19:15 -0700

    Buggy but eh

---
+++ DAND/PlayerStats.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+int main()
+{
+	/* 
+	4 d6 taking the highest 3 
+	temp dice holder
+	temp stat holder
+	player stats ( strength, charisma, dexterity, intelligence, constitution, wisdom )
+	*/
+	int tempDice[4];
+	int tempStat[6];
+	int playerStat[6];
+	
+	
+
+
+
+
+
+
+
+
+}
